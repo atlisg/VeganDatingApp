1f9ffa2ac88d5a413540c7e129c179c14dcf735e
Atli Sævar <atli@ankeri.is>
2022-04-07 19:50:21 +0000

    Initial Commit

---
+++ VeganDatingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VeganDatingApp
+//
+//  Created by Atli Saevar on 7.4.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
